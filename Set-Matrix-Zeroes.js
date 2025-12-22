2f2262e6c7c0a56a922a88ee757c953fa768f4d6
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2025-12-22 10:37:07 +0530

    [73] [Time Beats: 66.83%] [Memory Beats: 81.28%] - LeetPush

--- Set-Matrix-Zeroes.js
+++ Set-Matrix-Zeroes.js
@@ -7,31 +7,39 @@
 7  const c = matrix[0].length;
 8  const zeroPlaces = []; // [[row, column]]
 9
-10  // loop over matrix row by row
-11  for (let i = 0; i < r; i++) {
-12    for (let j = 0; j < c; j++) {
-13      if (matrix[i][j] === 0) {
-14        zeroPlaces.push([i, j]);
-15      }
-16    }
-17  }
-18
-19  // console.log(zeroPlaces);
-20  while (zeroPlaces.length !== 0) {
-21    const zeroPlace = zeroPlaces.pop();
+10  //optimization
+11  // const zeroRows = new Set();
+12  // const zeroCols = new Set();
+13
+14  // loop over matrix row by row
+15  for (let i = 0; i < r; i++) {
+16    for (let j = 0; j < c; j++) {
+17      if (matrix[i][j] === 0) {
+18        zeroPlaces.push([i, j]);
+19      }
+20    }
+21  }
 22
-23    // make row set to zero
-24    for (let i = 0; i < c; i++) {
-25      matrix[zeroPlace[0]][i] = 0;
-26    }
-27
-28    // make col set to zero
-29    for (let i = 0; i < r; i++) {
-30      matrix[i][zeroPlace[1]] = 0;
-31    }
-32  }
-33
-34  // console.log(matrix);
-35  return matrix;
-36 
-37};
+23  // console.log(zeroPlaces);
+24  let i = 0;
+25  while (i < zeroPlaces.length) {
+26    const zeroPlace = zeroPlaces[i];
+27    const currRow = zeroPlace[0];
+28    const currCol = zeroPlace[1];
+29
+30    // make row set to zero
+31    for (let i = 0; i < c; i++) {
+32      matrix[currRow][i] = 0;
+33    }
+34
+35    // make col set to zero
+36    for (let i = 0; i < r; i++) {
+37      matrix[i][currCol] = 0;
+38    }
+39
+40    i++;
+41  }
+42
+43  // console.log(matrix);
+44  return matrix;
+45};
