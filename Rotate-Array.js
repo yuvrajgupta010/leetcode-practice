68c824503bf99ab694930f00609aa32b17b1445c
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-01-07 22:17:01 +0530

    [189] [Time Beats: 84.74%] [Memory Beats: 97.85%] - LeetPush

--- Rotate-Array.js
+++ Rotate-Array.js
@@ -3,24 +3,29 @@
 3 * @param {number} k
 4 * @return {void} Do not return anything, modify nums in-place instead.
 5 */
-6var rotate = function(nums, k) {
-7   // Brute force
+6var rotate = function (nums, k) {
+7  const expectedRotation = k % nums.length;
 8
-9  const effectiveRotation = k % nums.length;
-10  const rotatedArr = [];
-11
-12  for (let i = nums.length - effectiveRotation; i < nums.length; i++) {
-13    rotatedArr.push(nums[i]);
-14  }
-15
-16  for (let j = 0; j < nums.length - effectiveRotation; j++) {
-17    rotatedArr.push(nums[j]);
-18  }
-19
-20  for (let p = 0; p < nums.length; p++) {
-21    nums[p] = rotatedArr[p];
-22  }
-23
-24  // console.log(nums, rotatedArr);
-25  // return nums;
-26};
+9  reverse(nums, 0, nums.length - 1);
+10
+11  reverse(nums, 0, expectedRotation - 1);
+12
+13  reverse(nums, expectedRotation, nums.length - 1);
+14
+15  // console.log(nums);
+16};
+17
+18function reverse(arr, start, end) {
+19  let i = start;
+20  let j = end;
+21
+22  while (i <= j) {
+23    let tem = arr[i];
+24    arr[i] = arr[j];
+25    arr[j] = tem;
+26
+27    i++;
+28    j--;
+29  }
+30}
+31
