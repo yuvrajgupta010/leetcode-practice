b0173eec730ebd93b4eda02d8424d27bc692fce3
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-01-16 23:07:34 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 51.37%] - LeetPush

--- Single-Element-in-a-Sorted-Array.js
+++ Single-Element-in-a-Sorted-Array.js
@@ -3,10 +3,22 @@
 3 * @return {number}
 4 */
 5var singleNonDuplicate = function(nums) {
-6      let currIdx = 0;
-7  while (currIdx < nums.length) {
-8    const currNum = nums[currIdx];
-9    if (currNum !== nums[currIdx + 1]) return currNum;
-10    currIdx += 2;
-11  }
-12};
+6    let left = 0;
+7    let right = nums.length - 1;
+8
+9    while (left < right) {
+10        let mid = left + Math.floor((right - left) / 2);
+11
+12        if (mid % 2 === 1) {
+13            mid--;
+14        }
+15
+16        if (nums[mid] === nums[mid + 1]) {
+17            left = mid + 2;
+18        } else {
+19            right = mid;
+20        }
+21    }
+22
+23    return nums[left]; 
+24};
