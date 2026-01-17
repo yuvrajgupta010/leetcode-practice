8a2529c5622d4cd8898ef63ccfad9fc90f68bdca
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-01-17 23:39:17 +0530

    [14] [Time Beats: 5.80%] [Memory Beats: 5.09%] - LeetPush

---
+++ Longest-Common-Prefix.js
@@ -0,0 +1,24 @@
+1/**
+2 * @param {string[]} strs
+3 * @return {string}
+4 */
+5var longestCommonPrefix = function(strs) {
+6      let longestPrefix = strs[0];
+7
+8  for (let i = 1; i < strs.length; i++) {
+9    if (!longestPrefix.length) break;
+10
+11    let updatedPrefix = "";
+12
+13    for (let j = 0; j < longestPrefix.length; j++) {
+14      if (longestPrefix[j] === strs[i][j]) updatedPrefix += longestPrefix[j];
+15      else {
+16        longestPrefix = updatedPrefix;
+17        continue;
+18      }
+19    }
+20  }
+21
+22  console.log(longestPrefix);
+23  return longestPrefix;
+24};
