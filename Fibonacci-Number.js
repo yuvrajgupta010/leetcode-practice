8b04e7310ab9b1039af5b6cbab1e84ca50879148
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-01-20 23:51:23 +0530

    [509] [Time Beats: 61.91%] [Memory Beats: 8.33%] - LeetPush

--- Fibonacci-Number.js
+++ Fibonacci-Number.js
@@ -5,11 +5,6 @@
 5var fib = function(n) {
 6      if (n === 0) return 0;
 7  if (n === 1) return 1;
-8  let a = 0,
-9    b = 1;
-10  for (let i = 2; i <= n; i++) {
-11    [a, b] = [b, a + b];
-12  }
-13  // console.log(b);
-14  return b;
-15};
+8
+9  return fib(n - 2) + fib(n - 1);
+10};
