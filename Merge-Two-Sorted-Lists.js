b2f4932d85a2fd263455e1e40848702e725780e0
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-02-04 22:48:58 +0530

    [21] [Time Beats: 100.00%] [Memory Beats: 61.54%] - LeetPush

--- Merge-Two-Sorted-Lists.js
+++ Merge-Two-Sorted-Lists.js
@@ -11,40 +11,21 @@
 11 * @return {ListNode}
 12 */
 13var mergeTwoLists = function(list1, list2) {
-14      let sortedList = new ListNode();
+14  let sortedList = new ListNode();
 15  let lastNode = sortedList;
 16
-17  let list1Node = list1;
-18  let list2Node = list2;
-19
-20  while (list1Node && list2Node) {
-21    if (list1Node.val < list2Node.val) {
-22      const newNode = new ListNode(list1Node.val);
-23      lastNode.next = newNode;
-24      lastNode = newNode;
-25      list1Node = list1Node.next;
-26    } else {
-27      const newNode = new ListNode(list2Node.val);
-28      lastNode.next = newNode;
-29      lastNode = newNode;
-30      list2Node = list2Node.next;
-31    }
-32  }
-33
-34  while (list1Node) {
-35    const newNode = new ListNode(list1Node.val);
-36    lastNode.next = newNode;
-37    lastNode = newNode;
-38    list1Node = list1Node.next;
-39  }
-40
-41  while (list2Node) {
-42    const newNode = new ListNode(list2Node.val);
-43    lastNode.next = newNode;
-44    lastNode = newNode;
-45    list2Node = list2Node.next;
-46  }
-47
-48  sortedList = sortedList.next;
-49  return sortedList;
-50};
+17  while (list1 && list2) {
+18    if (list1.val < list2.val) {
+19      lastNode.next = list1;
+20      list1 = list1.next;
+21    } else {
+22      lastNode.next = list2;
+23      list2 = list2.next;
+24    }
+25    lastNode = lastNode.next;
+26  }
+27
+28  lastNode.next = list1 || list2;
+29
+30  return sortedList.next;
+31};
