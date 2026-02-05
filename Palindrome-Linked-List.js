665fa60dbffa4b0888055f8c4181c406d3af5120
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-02-05 11:02:44 +0530

    [234] [Time Beats: 58.38%] [Memory Beats: 61.01%] - LeetPush

--- Palindrome-Linked-List.js
+++ Palindrome-Linked-List.js
@@ -1,31 +1,51 @@
-1/**
-2 * Definition for singly-linked list.
-3 * function ListNode(val, next) {
-4 *     this.val = (val===undefined ? 0 : val)
-5 *     this.next = (next===undefined ? null : next)
-6 * }
-7 */
-8/**
-9 * @param {ListNode} head
-10 * @return {boolean}
-11 */
-12var isPalindrome = function(head) {
-13  let stringForPalindrome = "";
+1const reverseLL = (head) => {
+2  let currHead = head;
+3  let lastHead = null;
+4
+5  while (currHead) {
+6    const nextHead = currHead.next;
+7    currHead.next = lastHead;
+8    lastHead = currHead;
+9    currHead = nextHead;
+10  }
+11
+12  return lastHead;
+13};
 14
-15  let currentNode = head;
-16  while (currentNode) {
-17    stringForPalindrome += currentNode.val;
-18    currentNode = currentNode.next;
-19  }
-20
-21  let i = 0;
-22  let j = stringForPalindrome.length - 1;
-23  while (i <= j) {
-24    if (stringForPalindrome[i] !== stringForPalindrome[j]) return false;
-25    i++;
-26    j--;
-27  }
-28
-29  return true;
-30
-31};
+15/**
+16 * Definition for singly-linked list.
+17 * function ListNode(val, next) {
+18 *     this.val = (val===undefined ? 0 : val)
+19 *     this.next = (next===undefined ? null : next)
+20 * }
+21 */
+22/**
+23 * @param {ListNode} head
+24 * @return {boolean}
+25 */
+26var isPalindrome = function(head) {
+27  let slow = head;
+28  let fast = head;
+29
+30  while (fast.next && fast.next.next) {
+31    slow = slow.next;
+32    fast = fast.next.next;
+33  }
+34
+35  // console.log(slow.next);
+36  const halfReverse = reverseLL(slow.next);
+37  // console.log(halfReverse);
+38
+39  let leftHead = head;
+40  let rightHead = halfReverse;
+41
+42  while (rightHead) {
+43    // console.log({ left: leftHead.val, right: rightHead.val });
+44    if (leftHead.val !== rightHead.val) return false;
+45    leftHead = leftHead.next;
+46    rightHead = rightHead.next;
+47  }
+48
+49  // console.log(slow);
+50  return true;
+51};
