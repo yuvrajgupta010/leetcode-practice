b1f7dbb12944790f8283559cbf6e2a4b84678578
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-02-14 17:39:08 +0530

    [496] [Time Beats: 51.75%] [Memory Beats: 92.65%] - LeetPush

--- Next-Greater-Element-I.js
+++ Next-Greater-Element-I.js
@@ -3,7 +3,7 @@
 3 * @param {number[]} nums2
 4 * @return {number[]}
 5 */
-6var nextGreaterElement = function (nums1, nums2) {
+6var nextGreaterElement = function(nums1, nums2) {
 7  let greaterNums = new Map();
 8  let stack = [];
 9
@@ -13,24 +13,16 @@
 13    }
 14    if (stack.length) {
 15      greaterNums.set(nums2[i], stack[stack.length - 1]);
-16      //   greaterNums[nums2[i]] = stack[stack.length - 1];
-17    } else {
-18      greaterNums.set(nums2[i], -1);
-19      //   greaterNums[nums2[i]] = -1;
-20    }
-21    stack.push(nums2[i]);
-22  }
-23
-24  //   console.log(greaterNums);
-25  let arrGreaterNums = [];
-26  for (let num of nums1) {
-27    // console.log(num, "outside", greaterNums.has(num));
-28
-29    if (greaterNums.has(num)) {
-30      //   console.log(num, "inside");
-31      arrGreaterNums.push(greaterNums.get(num));
-32    }
-33  }
-34
-35  return arrGreaterNums;
-36};
+16    } else {
+17      greaterNums.set(nums2[i], -1);
+18    }
+19    stack.push(nums2[i]);
+20  }
+21
+22  let arrGreaterNums = [];
+23  for (let num of nums1) {
+24    arrGreaterNums.push(greaterNums.get(num));
+25  }
+26
+27  return arrGreaterNums;
+28};
