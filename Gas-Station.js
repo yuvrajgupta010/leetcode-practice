587d6f22d0c2700e4c442cc4c43f81d2e06d36b7
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-02-17 23:15:48 +0530

    [134] [Time Beats: 85.47%] [Memory Beats: 26.24%] - LeetPush

--- Gas-Station.js
+++ Gas-Station.js
@@ -3,30 +3,19 @@
 3 * @param {number[]} cost
 4 * @return {number}
 5 */
-6var canCompleteCircuit = function (gas, cost) {
-7  let totalGas = 0,
-8    totalCost = 0;
-9
-10  for (let i = 0; i < gas.length; i++) {
-11    totalGas += gas[i];
-12    totalCost += cost[i];
-13  }
-14
-15  if (totalGas < totalCost) {
-16    return -1;
+6var canCompleteCircuit = function(gas, cost) {
+7    let total = 0, curr = 0, start = 0;
+8
+9for (let i = 0; i < gas.length; i++) {
+10  const diff = gas[i] - cost[i];
+11  total += diff;
+12  curr += diff;
+13
+14  if (curr < 0) {
+15    start = i + 1;
+16    curr = 0;
 17  }
-18
-19  let start = 0,
-20    currGas = 0;
-21
-22  for (let i = 0; i < gas.length; i++) {
-23    currGas += gas[i] - cost[i];
-24
-25    if (currGas < 0) {
-26      start = i + 1;
-27      currGas = 0;
-28    }
-29  }
-30
-31  return start;
-32};
+18}
+19
+20return total < 0 ? -1 : start;
+21};
