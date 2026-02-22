8d7ae6682fbd31b4eacee850d72a39d0b3cda54d
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-02-22 20:30:43 +0530

    [543] [Time Beats: 87.57%] [Memory Beats: 6.74%] - LeetPush

--- Diameter-of-Binary-Tree.js
+++ Diameter-of-Binary-Tree.js
@@ -10,21 +10,20 @@
 10 * @param {TreeNode} root
 11 * @return {number}
 12 */
-13var diameterOfBinaryTree = function (root) {
-14  let diameter = 0;
+13var diameterOfBinaryTree = function(root) {
+14      let diameter = 0;
 15
 16  const dfs = (node) => {
 17    if (!node) return 0;
 18
-19    const leftLength = node.left ? dfs(node.left) + 1 : 0;
-20    const rightLength = node.right ? dfs(node.right) + 1 : 0;
+19    const left = dfs(node.left);
+20    const right = dfs(node.right);
 21
-22    diameter = Math.max(leftLength + rightLength, diameter);
+22    diameter = Math.max(diameter, left + right);
 23
-24    return Math.max(leftLength, rightLength);
+24    return 1 + Math.max(left, right);
 25  };
 26
 27  dfs(root);
-28
-29  return diameter;
-30};
+28  return diameter;
+29};
