8dc6d7003aa48d8752b9176a55b6989b2a51efd2
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-02-23 23:33:50 +0530

    [102] [Time Beats: 100.00%] [Memory Beats: 68.38%] - LeetPush

--- Binary-Tree-Level-Order-Traversal.js
+++ Binary-Tree-Level-Order-Traversal.js
@@ -10,22 +10,42 @@
 10 * @param {TreeNode} root
 11 * @return {number[][]}
 12 */
-13var levelOrder = function (root) {
-14  if (!root) return [];
-15  const returnArr = [];
-16  const queue = [root];
+13// var levelOrder = function (root) {
+14//   if (!root) return [];
+15//   const returnArr = [];
+16//   const queue = [root];
 17
-18  while (queue.length) {
-19    const levelWidth = queue.length;
-20    const orderArr = [];
-21    for (let i = 0; i < levelWidth; i++) {
-22      const node = queue.shift();
-23      orderArr.push(node.val);
-24      if (node.left) queue.push(node.left);
-25      if (node.right) queue.push(node.right);
-26    }
-27    returnArr.push(orderArr);
-28  }
+18//   while (queue.length) {
+19//     const levelWidth = queue.length;
+20//     const orderArr = [];
+21//     for (let i = 0; i < levelWidth; i++) {
+22//       const node = queue.shift();
+23//       orderArr.push(node.val);
+24//       if (node.left) queue.push(node.left);
+25//       if (node.right) queue.push(node.right);
+26//     }
+27//     returnArr.push(orderArr);
+28//   }
 29
-30  return returnArr;
-31};
+30//   return returnArr;
+31// };
+32var levelOrder = function (root) {
+33  if (!root) return [];
+34  const returnArr = [];
+35
+36  const dfs = (node, depth) => {
+37    if (!node) return;
+38
+39    if (returnArr.length === depth) {
+40      returnArr.push([node.val]);
+41    } else {
+42      returnArr[depth].push(node.val);
+43    }
+44
+45    dfs(node.left, depth + 1);
+46    dfs(node.right, depth + 1);
+47  };
+48
+49  dfs(root, 0);
+50  return returnArr;
+51};
