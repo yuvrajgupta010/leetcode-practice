fc925313a5310fe81f3cd86986863de2995aa519
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-03-07 08:34:09 +0530

    [108] [Time Beats: 43.52%] [Memory Beats: 94.51%] - LeetPush

--- Convert-Sorted-Array-to-Binary-Search-Tree.js
+++ Convert-Sorted-Array-to-Binary-Search-Tree.js
@@ -11,25 +11,39 @@
 11 * @return {TreeNode}
 12 */
 13var sortedArrayToBST = function (nums) {
-14  let root = null;
+14  // let root = null;
 15
-16  const dfsPreOrder = (parent, l, r, isLeft) => {
-17    // console.log({ parent, l, r });
-18    if (l > r) return;
-19    const midIdx = Math.floor((l + r) / 2);
-20    const node = new TreeNode(nums[midIdx]);
-21    parent[isLeft ? "left" : "right"] = node;
-22    dfsPreOrder(node, l, midIdx - 1, true);
-23    dfsPreOrder(node, midIdx + 1, r, false);
-24  };
+16  // const dfsPreOrder = (parent, l, r, isLeft) => {
+17  //   // console.log({ parent, l, r });
+18  //   if (l > r) return;
+19  //   const midIdx = Math.floor((l + r) / 2);
+20  //   const node = new TreeNode(nums[midIdx]);
+21  //   parent[isLeft ? "left" : "right"] = node;
+22  //   dfsPreOrder(node, l, midIdx - 1, true);
+23  //   dfsPreOrder(node, midIdx + 1, r, false);
+24  // };
 25
-26  const midIdx = Math.floor(nums.length / 2);
-27  root = new TreeNode(nums[midIdx]);
+26  // const midIdx = Math.floor(nums.length / 2);
+27  // root = new TreeNode(nums[midIdx]);
 28
-29  // left
-30  dfsPreOrder(root, 0, midIdx - 1, true);
-31  // right
-32  dfsPreOrder(root, midIdx + 1, nums.length - 1, false);
+29  // // left
+30  // dfsPreOrder(root, 0, midIdx - 1, true);
+31  // // right
+32  // dfsPreOrder(root, midIdx + 1, nums.length - 1, false);
 33
-34  return root;
-35};
+34  // return root;
+35
+36  const dfsPreOrder = (l, r) => {
+37    if (l > r) return null;
+38
+39    const midIdx = Math.floor((l + r) / 2);
+40    const node = new TreeNode(nums[midIdx]);
+41
+42    node.left = dfsPreOrder(l, midIdx - 1);
+43    node.right = dfsPreOrder(midIdx + 1, r);
+44
+45    return node;
+46  };
+47
+48  return dfsPreOrder(0, nums.length - 1);
+49};
