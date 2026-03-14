65ff9032c432c0fee099fe0d06e0ddc2e443c971
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-03-14 18:41:57 +0530

    [173] [Time Beats: 87.83%] [Memory Beats: 88.13%] - LeetPush

--- Binary-Search-Tree-Iterator.js
+++ Binary-Search-Tree-Iterator.js
@@ -6,41 +6,43 @@
 6 *     this.right = (right===undefined ? null : right)
 7 * }
 8 */
-9/**
-10 * @param {TreeNode} root
-11 */
-12class BSTIterator {
-13  constructor(root) {
-14    this.inorderValues = [];
-15    this.currIdx = 0;
-16
-17    const dfs = (node) => {
-18      if (!node) return;
+9class BSTIterator {
+10  constructor(root) {
+11    this.stack = [];
+12
+13    let curr = root;
+14    while (curr) {
+15      this.stack.push(curr);
+16      curr = curr.left;
+17    }
+18  }
 19
-20      dfs(node.left);
-21      this.inorderValues.push(node.val);
-22      dfs(node.right);
-23    };
-24
-25    dfs(root);
-26  }
-27  /**
-28   * @return {number}
-29   */
-30  next() {
-31    return this.inorderValues[this.currIdx++];
-32  }
-33  /**
-34   * @return {boolean}
-35   */
-36  hasNext() {
-37    return this.currIdx < this.inorderValues.length;
-38  }
-39}
-40
-41/**
-42 * Your BSTIterator object will be instantiated and called as such:
-43 * var obj = new BSTIterator(root)
-44 * var param_1 = obj.next()
-45 * var param_2 = obj.hasNext()
-46 */
+20  /**
+21   * @return {number}
+22   */
+23  next() {
+24    const node = this.stack.pop();
+25
+26    let curr = node.right;
+27    while (curr) {
+28      this.stack.push(curr);
+29      curr = curr.left;
+30    }
+31
+32    return node.val;
+33  }
+34
+35  /**
+36   * @return {boolean}
+37   */
+38  hasNext() {
+39    return !!this.stack.length;
+40  }
+41}
+42
+43/** 
+44 * Your BSTIterator object will be instantiated and called as such:
+45 * var obj = new BSTIterator(root)
+46 * var param_1 = obj.next()
+47 * var param_2 = obj.hasNext()
+48 */
