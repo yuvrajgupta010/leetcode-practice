16e21a34ec7604281e95f87e14691b27bc1219c0
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-03-18 23:45:50 +0530

    [74] [Time Beats: 100.00%] [Memory Beats: 19.81%] - LeetPush

--- Search-a-2D-Matrix.js
+++ Search-a-2D-Matrix.js
@@ -1,37 +1,30 @@
 1
-2const binarySearch = (arr, t) => {
-3  let i = 0;
-4  let j = arr.length - 1;
-5
-6  while (i <= j) {
-7    const midIdx = Math.floor((i + j) / 2);
-8
-9    const midValue = arr[midIdx];
-10    if (midValue === t) return true;
+2/**
+3 * @param {number[][]} matrix
+4 * @param {number} target
+5 * @return {boolean}
+6 */
+7var searchMatrix = function (matrix, target) {
+8  let m = matrix[0].length;
+9  let i = 0;
+10  let j = matrix.length * matrix[0].length - 1;
 11
-12    if (midValue > t) {
-13      j = midIdx - 1;
-14    } else {
-15      i = midIdx + 1;
-16    }
-17  }
+12  while (i <= j) {
+13    const midIdx = Math.floor((i + j) / 2);
+14
+15    // find cordinates
+16    const row = Math.floor(midIdx / m);
+17    const col = midIdx % m;
 18
-19  return false;
-20};
+19    const midValue = matrix[row][col];
+20    if (midValue === target) return true;
 21
-22/**
-23 * @param {number[][]} matrix
-24 * @param {number} target
-25 * @return {boolean}
-26 */
-27var searchMatrix = function (matrix, target) {
-28  let m = matrix.length;
-29
-30  for (let i = 0; i < m; i++) {
-31    if (matrix[i].at(0) <= target && matrix[i].at(-1) >= target) {
-32      return binarySearch(matrix[i], target);
-33    }
-34  }
-35
-36  return false;
-37};
+22    if (midValue > target) {
+23      j = midIdx - 1;
+24    } else {
+25      i = midIdx + 1;
+26    }
+27  }
+28
+29  return false;
+30};
