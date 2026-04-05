93225dcbd9ba52b6eaa889c6956cc4cf2cfd9f57
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-04-05 19:07:55 +0530

    [1584] [Time Beats: 13.27%] [Memory Beats: 57.71%] - LeetPush

--- Min-Cost-to-Connect-All-Points.js
+++ Min-Cost-to-Connect-All-Points.js
@@ -50,60 +50,47 @@
 50        if (this.heap[leftChildIdx][0] > this.heap[rightChildIdx][0])
 51          minIdx = rightChildIdx;
 52
-53        if (this.heap[currIdx][0] > this.heap[minIdx][0])
+53        if (this.heap[currIdx][0] > this.heap[minIdx][0]) {
 54          [this.heap[minIdx], this.heap[currIdx]] = [
 55            this.heap[currIdx],
 56            this.heap[minIdx],
 57          ];
-58        currIdx = minIdx;
-59      }
-60    }
-61
-62    return node;
-63  }
-64}
-65
-66const findMinCostToConnect = (wightedGraph) => {
-67  const mstSet = new Set();
-68  const queue = new MinPQ();
-69  let cost = 0;
-70  queue.enqueue([0, 0]);
-71
-72  while (queue.heap.length) {
-73    const [w, v] = queue.dequeue();
-74
-75    if (mstSet.has(v)) continue;
+58          currIdx = minIdx;
+59        } else break;
+60      }
+61    }
+62
+63    return node;
+64  }
+65}
+66
+67/**
+68 * @param {number[][]} points
+69 * @return {number}
+70 */
+71var minCostConnectPoints = function (points) {
+72  const mstSet = new Set();
+73  const queue = new MinPQ();
+74  let cost = 0;
+75  queue.enqueue([0, 0]);
 76
-77    mstSet.add(v);
-78    cost += w;
+77  while (queue.heap.length) {
+78    const [w, v] = queue.dequeue();
 79
-80    for (let node of wightedGraph[v]) {
-81      if (!mstSet.has(node[1])) {
-82        queue.enqueue(node);
-83      }
-84    }
-85  }
-86
-87  return cost;
-88};
-89
-90/**
-91 * @param {number[][]} points
-92 * @return {number}
-93 */
-94var minCostConnectPoints = function (points) {
-95  const wightedGraph = [];
-96
-97  for (let i = 0; i < points.length; i++) {
-98    const connections = [];
-99    for (let j = 0; j < points.length; j++) {
-100      if (i === j) continue;
-101      const x = Math.abs(points[j][0] - points[i][0]);
-102      const y = Math.abs(points[j][1] - points[i][1]);
-103      connections.push([x + y, j]);
-104    }
-105    wightedGraph.push(connections);
-106  }
-107
-108  return findMinCostToConnect(wightedGraph);
-109};
+80    if (mstSet.has(v)) continue;
+81
+82    mstSet.add(v);
+83    cost += w;
+84
+85    for (let i = 0; i < points.length; i++) {
+86      if (v === i) continue;
+87      if (!mstSet.has(i)) {
+88        const x = Math.abs(points[v][0] - points[i][0]);
+89        const y = Math.abs(points[v][1] - points[i][1]);
+90        queue.enqueue([x + y, i]);
+91      }
+92    }
+93  }
+94
+95  return cost;
+96};
