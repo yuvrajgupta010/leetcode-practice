bdda4a4fd4a95f4f085bd70602e864bb7a5a7780
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-03 18:54:35 +0530

    [416] [Time Beats: 13.97%] [Memory Beats: 33.54%] - LeetPush

--- Partition-Equal-Subset-Sum.js
+++ Partition-Equal-Subset-Sum.js
@@ -1,31 +1,34 @@
-1const fRecursive = (idx, target, arr, memo) => {
-2  if (target === 0) return true;
-3  if (idx === 0) return arr[0] === target;
-4  if (memo[idx][target] !== -1) return memo[idx][target];
-5
-6  let notTake = fRecursive(idx - 1, target, arr, memo);
-7  let taken = false;
-8  if (target >= arr[idx]) {
-9    taken = fRecursive(idx - 1, target - arr[idx], arr, memo);
-10  }
-11
-12  return (memo[idx][target] = taken || notTake);
-13};
-14
-15/**
-16 * @param {number[]} nums
-17 * @return {boolean}
-18 */
-19var canPartition = function (nums) {
-20  const n = nums.length;
-21  let k = nums.reduce((acc, curr) => acc + curr, 0);
-22
-23  if (k % 2) return false;
-24  k = k / 2;
-25
-26  const memoize = Array.from({ length: n }, () =>
-27    Array.from({ length: k + 1 }, () => -1),
-28  );
-29
-30  return fRecursive(n - 1, k, nums, memoize);
-31};
+1
+2/**
+3 * @param {number[]} nums
+4 * @return {boolean}
+5 */
+6var canPartition = function (nums) {
+7  const n = nums.length;
+8  let k = nums.reduce((acc, curr) => acc + curr, 0);
+9
+10  if (k % 2) return false;
+11  k = k / 2;
+12
+13  const dp = Array.from({ length: n }, () =>
+14    Array.from({ length: k + 1 }, () => 0),
+15  );
+16
+17  for (let i = 0; i < n; i++) {
+18    dp[i][0] = true;
+19  }
+20  dp[0][nums[0]] = true;
+21
+22  for (let idx = 1; idx < n; idx++) {
+23    for (let target = 1; target <= k; target++) {
+24      const notTake = dp[idx - 1][target];
+25      let take = false;
+26      if (nums[idx] <= target) {
+27        take = dp[idx - 1][target - nums[idx]];
+28      }
+29      dp[idx][target] = notTake || take;
+30    }
+31  }
+32
+33  return dp[n - 1][k];
+34};
