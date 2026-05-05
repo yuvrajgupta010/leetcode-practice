91085106654229be5b3f902b3d79b2ac363f967b
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-05 09:02:24 +0530

    [494] [Time Beats: 45.10%] [Memory Beats: 32.60%] - LeetPush

--- Target-Sum.js
+++ Target-Sum.js
@@ -1,19 +1,32 @@
-1const solve = (idx, currSum, nums, n, target) => {
+1const solve = (idx, currSum, nums, n, target, dp, sum) => {
 2  if (idx === n) {
 3    if (currSum === target) return 1;
 4    return 0;
 5  }
-6  const onSubtract = solve(idx + 1, currSum - nums[idx], nums, n, target);
-7  const onAdd = solve(idx + 1, currSum + nums[idx], nums, n, target);
-8  return onSubtract + onAdd;
-9};
-10
-11/**
-12 * @param {number[]} nums
-13 * @param {number} target
-14 * @return {number}
-15 */
-16var findTargetSumWays = function (nums, target) {
-17  const n = nums.length;
-18  return solve(0, 0, nums, n, target);
-19};
+6  if (dp[idx][currSum + sum] !== -1) return dp[idx][currSum + sum];
+7  const onSubtract = solve(
+8    idx + 1,
+9    currSum - nums[idx],
+10    nums,
+11    n,
+12    target,
+13    dp,
+14    sum,
+15  );
+16  const onAdd = solve(idx + 1, currSum + nums[idx], nums, n, target, dp, sum);
+17  return (dp[idx][currSum + sum] = onSubtract + onAdd);
+18};
+19
+20/**
+21 * @param {number[]} nums
+22 * @param {number} target
+23 * @return {number}
+24 */
+25var findTargetSumWays = function (nums, target) {
+26  const n = nums.length;
+27  const sum = nums.reduce((sum, num) => sum + num, 0);
+28  const dp = Array.from({ length: n }, () =>
+29    Array.from({ length: 2 * sum + 1 }, () => -1),
+30  );
+31  return solve(0, 0, nums, n, target, dp, sum);
+32};
