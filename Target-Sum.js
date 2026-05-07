70a181fad7076601596c65771eda59ce6c979473
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-07 08:21:05 +0530

    [494] [Time Beats: 76.41%] [Memory Beats: 36.61%] - LeetPush

--- Target-Sum.js
+++ Target-Sum.js
@@ -1,32 +1,38 @@
-1const solve = (idx, currSum, nums, n, target, dp, sum) => {
-2  if (idx === n) {
-3    if (currSum === target) return 1;
-4    return 0;
-5  }
-6  if (dp[idx][currSum + sum] !== -1) return dp[idx][currSum + sum];
-7  const onSubtract = solve(
-8    idx + 1,
-9    currSum - nums[idx],
-10    nums,
-11    n,
-12    target,
-13    dp,
-14    sum,
+1/**
+2 * @param {number[]} nums
+3 * @param {number} target
+4 * @return {number}
+5 */
+6var findTargetSumWays = function(nums, target) {
+7      const n = nums.length;
+8  const sum = nums.reduce((a, b) => a + b, 0);
+9
+10  // Edge case
+11  if (Math.abs(target) > sum) return 0;
+12
+13  const dp = Array.from({ length: n }, () =>
+14    Array(2 * sum + 1).fill(0)
 15  );
-16  const onAdd = solve(idx + 1, currSum + nums[idx], nums, n, target, dp, sum);
-17  return (dp[idx][currSum + sum] = onSubtract + onAdd);
-18};
-19
-20/**
-21 * @param {number[]} nums
-22 * @param {number} target
-23 * @return {number}
-24 */
-25var findTargetSumWays = function (nums, target) {
-26  const n = nums.length;
-27  const sum = nums.reduce((sum, num) => sum + num, 0);
-28  const dp = Array.from({ length: n }, () =>
-29    Array.from({ length: 2 * sum + 1 }, () => -1),
-30  );
-31  return solve(0, 0, nums, n, target, dp, sum);
-32};
+16
+17  // Base case
+18  dp[0][nums[0] + sum] += 1;
+19  dp[0][-nums[0] + sum] += 1;
+20
+21  // Fill table
+22  for (let i = 1; i < n; i++) {
+23    for (let s = -sum; s <= sum; s++) {
+24      const prevIdx = s + sum;
+25
+26      if (dp[i - 1][prevIdx] !== 0) {
+27        const add = s + nums[i];
+28        const subtract = s - nums[i];
+29
+30        dp[i][add + sum] += dp[i - 1][prevIdx];
+31        dp[i][subtract + sum] += dp[i - 1][prevIdx];
+32      }
+33    }
+34  }
+35
+36  return dp[n - 1][target + sum] || 0;
+37
+38};
