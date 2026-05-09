d190add5f31f78fa0436d1efebd5ce14b84650b1
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-10 01:17:32 +0530

    [1143] [Time Beats: 9.83%] [Memory Beats: 39.85%] - LeetPush

--- Longest-Common-Subsequence.js
+++ Longest-Common-Subsequence.js
@@ -1,21 +1,23 @@
 1var longestCommonSubsequence = function (text1, text2) {
-2  const dp = Array.from({ length: text1.length }, () =>
-3    Array.from({ length: text2.length }, () => -1),
-4  );
-5
-6  const solve = (idx1, idx2) => {
-7    if (idx1 < 0 || idx2 < 0) return 0;
+2  const n = text1.length;
+3  const m = text2.length;
+4
+5  const dp = Array.from({ length: n + 1 }, () =>
+6    Array.from({ length: m + 1 }, () => -1),
+7  );
 8
-9    if (dp[idx1][idx2] !== -1) return dp[idx1][idx2];
-10    if (text1[idx1] === text2[idx2]) {
-11      return (dp[idx1][idx2] = 1 + solve(idx1 - 1, idx2 - 1));
-12    } else {
-13      return (dp[idx1][idx2] = Math.max(
-14        solve(idx1 - 1, idx2),
-15        solve(idx1, idx2 - 1),
-16      ));
-17    }
-18  };
-19
-20  return solve(text1.length - 1, text2.length - 1);
-21};
+9  for (let i = 0; i <= n; i++) dp[i][0] = 0;
+10  for (let j = 0; j <= m; j++) dp[0][j] = 0;
+11
+12  for (let i = 1; i <= n; i++) {
+13    for (let j = 1; j <= m; j++) {
+14      if (text1[i - 1] === text2[j - 1]) {
+15        dp[i][j] = 1 + dp[i - 1][j - 1];
+16      } else {
+17        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+18      }
+19    }
+20  }
+21
+22  return dp[n][m];
+23};
