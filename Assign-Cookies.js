d2e3f3eea287fa60f0d6dd84124b9a0d8643626d
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-15 08:54:54 +0530

    [455] [Time Beats: 92.93%] [Memory Beats: 50.64%] - LeetPush

--- Assign-Cookies.js
+++ Assign-Cookies.js
@@ -3,20 +3,23 @@
 3 * @param {number[]} s
 4 * @return {number}
 5 */
-6var findContentChildren = function(g, s) {
-7    g.sort((a, b) => a - b);
+6var findContentChildren = function (g, s) {
+7  g.sort((a, b) => a - b);
 8  s.sort((a, b) => a - b);
-9  let satisfiedKids = 0;
-10  let gPointer = 0,
-11    sPointer = 0;
-12
-13  while (gPointer < g.length && sPointer < s.length) {
-14    if (s[sPointer] >= g[gPointer]) {
-15      gPointer++;
-16      satisfiedKids++;
-17    } 
-18      sPointer++;
-19  }
-20  // console.log(satisfiedKids);
-21  return satisfiedKids;
-22};
+9
+10  let i = g.length,
+11    j = s.length,
+12    count = 0;
+13
+14  while (i && j) {
+15    if (g[i - 1] <= s[j - 1]) {
+16      count++;
+17      i--;
+18      j--;
+19    } else {
+20      i--;
+21    }
+22  }
+23
+24  return count;
+25};
