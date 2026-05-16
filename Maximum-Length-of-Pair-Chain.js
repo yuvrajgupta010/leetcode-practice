ccab459e4bdc7b48049eb84825fb54064dd9a2d9
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-16 09:29:52 +0530

    [646] [Time Beats: 85.58%] [Memory Beats: 92.31%] - LeetPush

--- Maximum-Length-of-Pair-Chain.js
+++ Maximum-Length-of-Pair-Chain.js
@@ -4,22 +4,17 @@
 4 */
 5var findLongestChain = function (pairs) {
 6  const n = pairs.length;
-7  let maxCount = 1;
-8  pairs.sort((a, b) => a[1] - b[1]);
-9
-10  for (let i = 0; i < n - 1; i++) {
-11    let countOfChain = 1;
-12    let prevIdx = i;
-13
-14    for (let j = i + 1; j < n; j++) {
-15      if (pairs[prevIdx][1] < pairs[j][0]) {
-16        countOfChain++;
-17        prevIdx = j;
-18      }
-19    }
-20
-21    maxCount = maxCount < countOfChain ? countOfChain : maxCount;
-22  }
-23
-24  return maxCount;
-25};
+7  pairs.sort((a, b) => a[1] - b[1]);
+8
+9  let countOfChain = 1;
+10  let prevIdx = 0;
+11
+12  for (let j = 1; j < n; j++) {
+13    if (pairs[prevIdx][1] < pairs[j][0]) {
+14      countOfChain++;
+15      prevIdx = j;
+16    }
+17  }
+18
+19  return countOfChain;
+20};
