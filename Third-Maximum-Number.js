68a256997f173a6364cff99928128b4bea7de6ea
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-21 09:14:58 +0530

    [414] [Time Beats: 18.77%] [Memory Beats: 7.32%] - LeetPush

--- Third-Maximum-Number.js
+++ Third-Maximum-Number.js
@@ -8,17 +8,17 @@
 8
 9  for (let num of nums) {
 10    if (!map.has(num)) {
-11      for (let i = 0; i < tracker.length; i++) {
-12        if (tracker[i] < num) {
-13          // code
-14          tracker.splice(i, 0, num);
-15          tracker.pop();
-16          map.add(num);
+11      map.add(num);
+12      for (let i = 0; i < tracker.length; i++) {
+13        if (tracker[i] < num) {
+14          // code
+15          tracker.splice(i, 0, num);
+16          tracker.pop();
 17          break;
 18        }
 19      }
 20    }
 21  }
 22
-23  return tracker.at(-1) === -Infinity ? tracker[0] : tracker.at(-1);
+23  return tracker[2] === -Infinity ? tracker[0] : tracker[2];
 24};
