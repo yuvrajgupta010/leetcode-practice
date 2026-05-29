948a2f08888055359e52062688e17079c519d364
Yuvraj Gupta <83709965+yuvrajgupta010@users.noreply.github.com>
2026-05-29 22:07:51 +0530

    [209] [Time Beats: 29.30%] [Memory Beats: 43.88%] - LeetPush

--- Minimum-Size-Subarray-Sum.js
+++ Minimum-Size-Subarray-Sum.js
@@ -3,22 +3,21 @@
 3 * @param {number[]} nums
 4 * @return {number}
 5 */
-6var minSubArrayLen = function(target, nums) {
-7 let n = nums.length,
-8    prevIdx = 0,
-9    sum = 0,
-10    minLength = Infinity;
-11
-12  for (let currIdx = 0; currIdx < n; currIdx++) {
-13    sum += nums[currIdx];
-14
-15    while (sum >= target) {
-16      const expectedMinLength = currIdx - prevIdx + 1;
-17      minLength = Math.min(minLength, expectedMinLength);
-18      // console.log(sum);
-19      sum -= nums[prevIdx++];
-20    }
-21  }
-22
-23  return minLength === Infinity ? 0 : minLength;
-24};
+6var minSubArrayLen = function (target, nums) {
+7  let i = 0,
+8    j = 0,
+9    n = nums.length - 1,
+10    sum = nums[0],
+11    minLength = Infinity;
+12
+13  while (i <= n && j <= n) {
+14    if (sum >= target) {
+15      minLength = Math.min(minLength, j + 1 - i);
+16      sum -= nums[i++];
+17    } else {
+18      sum += nums[++j];
+19    }
+20  }
+21
+22  return minLength === Infinity ? 0 : minLength;
+23};
