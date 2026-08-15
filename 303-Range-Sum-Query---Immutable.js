8abbd15f90a267108ac71df646f4a9ee999e1b20
Yuvraj Gupta <83709965+yuvrajgupta036@users.noreply.github.com>
2026-08-15 17:49:35 +0530

    [303] [Time Beats: 14.81%] [Memory Beats: 98.27%] - LeetPush

---
+++ 303-Range-Sum-Query---Immutable.js
@@ -0,0 +1,21 @@
+class NumArray {
+  constructor(nums) {
+    this.nums = nums;
+  }
+
+  sumRange(left, right) {
+    let sum = 0;
+
+    for (let i = left; i <= right; i++) {
+      sum += this.nums[i];
+    }
+
+    return sum;
+  }
+}
+
+/** 
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(left,right)
+ */
